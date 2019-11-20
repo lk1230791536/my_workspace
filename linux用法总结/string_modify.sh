53f1e6d8d035e77c8ba2a25dec12f63e04301b9b
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2019-11-20 15:10:42 +0800

    Update string_modify.sh

--- linux用法总结/string_modify.sh
+++ linux用法总结/string_modify.sh
@@ -3,10 +3,10 @@
 function sed(){
   ##(如果sed中有变量，将单引号变为双引号)
   #查找指定内容的行号
-  LINE_NUM=$(sed -n '/xxxx/='  aaa.txt)
+  LINE_NUM=$(sed -n -e '/xxxx/='  aaa.txt)
   #查找范围内指定的行号
   LINE_NUM=`sed -n "/start/,/target/=" aaa.txt |tail -n1`
-  #显示特定行号
+  #显示特定行号内容
   awk 'NR==4 {print}' aaa.txt
   #根据已知行号整行替换（行头有两个空格）
   sed -i $LINE_NUM'c \  xxxx' aaa.txt
@@ -18,12 +18,16 @@ function sed(){
   #替换行内容
   sed -i '/aa/c bb' aaa.txt
   #替换变量
-    sed -i s"/ip/$ip/g" *.txt
+  sed -i s"/ip/$ip/g" *.txt
+  #特定行号后追加内容
+  sed -i ${LINE_NUM}'s/$/content/' aaa.txt
 }
 function variable(){
   aa='cjk-fda-123'
   echo ${aa/-/#}  #/匹配第一个
   echo ${aa//-/#} #/贪婪匹配全部
   echo ${aa#*fda} #截取fda右测所有
+  echo ${aa:2:3}  #根据游标截取
+  echo ${#aa} #显示字符串长度
 }
 
