9ca0f728e270a61b4333f6d4211e280861f8a7bf
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2019-11-20 14:22:44 +0800

    Update command.sh

--- linux用法总结/command.sh
+++ linux用法总结/command.sh
@@ -7,6 +7,14 @@ function other(){
 function commands(){
   #强制复制
   \cp -f *.txt tempry/
+  #修改用户密码
+  chage -l testuser #查看有效期
+  chage -M 99999 testuser #修改为永久
+  #系统时间
+  date +%Y%m%d  #显示当天年月日
+  date +%Y%m%d --date="+1 day"  #显示前一天的日期
+  date +%Y%m%d --date="-1 day"  #显示后一天的日期
+  date +%s  #从 1970 年 1 月 1 日 00:00:00 UTC 到目前为止的秒数（时间戳）可用于判断时间过期
 }
 function search(){
   find $installDir/tomcat_*/webapps/*/WEB-INF  -name 'bootstrap.yml'
