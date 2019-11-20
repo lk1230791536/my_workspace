6ae02130da33bdd7b5fad46bf92bd65b5f741b3e
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2019-11-20 10:03:55 +0800

    Update command.sh

--- linux用法总结/command.sh
+++ linux用法总结/command.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+function other(){
+  #生成随机数
+  systemid_temp=`cat /proc/sys/kernel/random/uuid`
+  systemid=${systemid_temp//-/}
+}
+function commands(){
+  #强制复制
+  \cp -f *.txt tempry/
+}
+function search(){
+  find $installDir/tomcat_*/webapps/*/WEB-INF  -name 'bootstrap.yml'
+}
