9871a986e2af9a36869834a59585366538014b67
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2020-03-11 18:06:45 +0800

    Update command2.sh

--- linux用法总结/command2.sh
+++ linux用法总结/command2.sh
@@ -1,1 +1,7 @@
+##flock文件锁之实际运用
+#前面未获取到锁直接返回 直到其他运行完毕 这个才开始运行
+#-x写入锁（排他锁）;-n非阻塞模式
+* * * * * (flock -xn ./test.lock -c "sh /root/test.sh") #crontab运用flock防止重复执行
 
+##timeout运行指定的命令，如果在指定时间后仍在运行，则杀死该进程
+timeout 10 command
