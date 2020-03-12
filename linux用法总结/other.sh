bca6f734a729634f7adc859fc3dbcb249f065c19
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2020-03-12 16:33:46 +0800

    Update other.sh

--- linux用法总结/other.sh
+++ linux用法总结/other.sh
@@ -1,8 +1,8 @@
-##修改后台为中文
+######修改后台为中文
 vim vim /etc/locale.conf
 LANG="zh_CN.UTF-8"
 
-##生成随机数
+######生成随机数
 #方法一：通过系统环境变量($RANDOM)实现：
 echo "$RANDOM"|md5sum|cut -c 5-15
 #方法二：通过openssl产生随机数，示例：
@@ -14,3 +14,19 @@ cat /proc/sys/kernel/random/uuid
 uuidgen
 #上面的随机数长短不一,可以使用md5sum统一格式：
 uuidgen|md5sum|cut -c 2-10
+
+######进度条
+printf #格式化输出："-"表示左对齐, "10 10 4 4" 表示占的字符位数；%s字符串；%%表示%本身
+#!/bin/bash
+function Proceess(){
+spa=''
+i=0
+while [ $i -le 100 ]
+do
+ printf "[%-50s] %d%% \r" "$spa" "$i";
+ sleep 0.5
+ ((i=i+2))
+ spa+='#'
+done
+echo
+}
