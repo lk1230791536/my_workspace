b641c22ae3b7f507015150e72ba325f892a4d921
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2020-03-13 09:34:59 +0800

    Update prc.sh

--- prc.sh
+++ prc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 #chkconfig: 2345 80 90
 #description:test
+##"使用&后台运行程序：
+#结果会输出到终端
+#使用Ctrl + C发送SIGINT信号，程序免疫
+#关闭session发送SIGHUP信号，程序关闭
+#使用nohup运行程序：
+#结果默认会输出到nohup.out
+#使用Ctrl + C发送SIGINT信号，程序关闭
+#关闭session发送SIGHUP信号，程序免疫
+#平日线上经常使用nohup和&配合来启动程序：
+#同时免疫SIGINT和SIGHUP信号"
 nohup /bin/bash /root/lk.sh >> /root/log &
 exit
