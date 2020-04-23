5ce430d76b73961318b9289014b03a70865cdf1d
lk1320791536 <49162861+lk1230791536@users.noreply.github.com>
2020-04-23 19:48:33 +0800

    Update module_01.py

--- python总结/module_01.py
+++ python总结/module_01.py
@@ -44,3 +44,12 @@ with open('example.json','w',encoding='utf-8') as f:
     json.dump(bb,f) #写入json文件
 f = open('example.json','r',encoding='utf-8')
 info = json.load(f) #读取json文件
+
+###xml文件读取
+#使用xml.dom.minidom来解析xml文件
+from xml.dom import minidom
+root = minidom.parse("server.xml")  #读取文件
+collection = DOMTree.documentElement  #解析文件
+connectorNode = root.getElementsByTagName("Connector")[0] #定位到匹配的标签名
+port = connectorNode.getAttribute("port") #获取标签中的对应属性值
+detail = port.childNodes[0].data  #获取标签中的数据
